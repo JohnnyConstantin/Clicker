b038d8dbcd212e9e03883cf728dbfd0c84e93ddd
Krasav4ik <zub4encko.kostya@yandex.ru>
2020-03-14 21:37:07 +0300

    Some useless shit

--- app/src/main/java/com/example/clickerapp/MainActivity.java
+++ app/src/main/java/com/example/clickerapp/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.clickerapp;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new TestSurfaceView(this));
     }
 }
-//sdfwrfrwfw
+class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+
+    public TestSurfaceView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        Paint.setColor
+        Canvas.drawColor();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+}
