c1c03c63e5cf7e9acaec1d8e69a8caccd5fa0f4a
Krasav4ik <zub4encko.kostya@yandex.ru>
2020-03-16 21:01:38 +0300

    Начал верстку

--- app/src/main/java/com/example/clickerapp/MainActivity.java
+++ app/src/main/java/com/example/clickerapp/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.clickerapp;
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -16,28 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new TestSurfaceView(this));
-    }
-}
-class TestSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
-
-    public TestSurfaceView(Context context) {
-        super(context);
-        getHolder().addCallback(this);
-        Paint.setColor
-        Canvas.drawColor();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
+        setContentView(R.layout.activity_main);
     }
 }
